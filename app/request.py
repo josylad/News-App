04aa860e7543975456a007d968dec0e7e23177be
Joseph Adediji <josylado@gmail.com>
2019-09-09 12:18:36 +0300

    Add function for getting news articles

--- app/request.py
+++ app/request.py
@@ -26,3 +26,39 @@ def get_sources():
         return sources
 
 
+def get_news(id):
+    print("***********************************")
+    print("I am getting the news article")    
+    news_list = []
+    articles_url = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'.format(id, API_KEY)
+    response = requests.get(articles_url)
+    if response.status_code == 200:
+        response_json =  response.json()
+        
+        articles = response_json['articles']
+        
+        for data in articles:
+            url = data.get('url')
+            id = data.get('id')
+            source = data.get('source')
+            name = source.get('name')
+            title = data.get('title')
+            description =  data.get('description')
+            author = data.get('author')
+            time = data.get('publishedAt')
+            urlToImage = data.get('urlToImage')
+
+            news_article = NewsArticles(url, id, name, description, author, title, urlToImage,time)
+            news_list.append(news_article)
+            print("***********************************")
+            print(name)
+            print("***********************************")
+        
+        return news_list
+    
+
+
+def get_article(self, article):
+    pass
+
+
