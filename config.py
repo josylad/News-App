90aa7b4b1d4d36a35329d6017e47185c6df17c0e
Joseph Adediji <josylado@gmail.com>
2019-09-09 12:19:27 +0300

    Add dotenv to config file

--- config.py
+++ config.py
@@ -4,37 +4,3 @@ from dotenv import load_dotenv
 load_dotenv() 
 
 
-class Config:
-    '''
-    General configuration parent class
-    '''
-    API_BASE_URL ='https://newsapi.org/v2/sources?language=en&apiKey={}'
-    API_KEY = os.environ.get('API_KEY')
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-
-
-class ProdConfig(Config):
-    '''
-    Production  configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-    pass
-
-
-class DevConfig(Config):
-    '''
-    Development  configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-
-    DEBUG = True
-
-
-config_options = {
-'development': DevConfig,
-'production': ProdConfig
-}
