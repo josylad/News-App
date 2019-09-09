9ba384677491c98c859ec18fdcbf40cdc9ea119e
Joseph Adediji <josylado@gmail.com>
2019-09-09 12:20:31 +0300

    Add ProdConfig class to the config.py file

--- config.py
+++ config.py
@@ -13,3 +13,13 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
